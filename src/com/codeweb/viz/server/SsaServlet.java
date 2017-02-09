d57685769a9b2e3c4ab0de2af86325ea1e53a185
Ben Brichetti <brichetti.ben@idsi.com>
2017-02-09 15:12:47 -0500

    Cleanup.

--- src/com/codeweb/viz/server/SsaServlet.java
+++ src/com/codeweb/viz/server/SsaServlet.java
@@ -23,6 +23,37 @@ public class SsaServlet extends HttpServlet
 {
   private static final long serialVersionUID = -6580386317663342003L;
 
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+  {
+    ProjectStructure project = FileIO.read(
+        new File(getServletContext().getRealPath("data/ssa/Fibonacci.ssa")), ProjectStructure.class);
+
+    PrintWriter out = resp.getWriter();
+
+    JSONObject responseObj = new JSONObject();
+    try
+    {
+      JSONArray nodesArray = new JSONArray();
+      JSONArray edgesArray = new JSONArray();
+      Collection<ProjectPackage> packages = project.getTopPackages();
+      for (ProjectPackage pkg : packages)
+      {
+        createNetworkNodesAndEdges(nodesArray, edgesArray, pkg, null);
+      }
+      responseObj.put("projName", project.getProjName());
+      responseObj.put("nodes", nodesArray);
+      responseObj.put("edges", edgesArray);
+    }
+    catch (JSONException e)
+    {
+      throw new ServletException(e);
+    }
+
+    out.write(responseObj.toString());
+    out.flush();
+  }
+
   private static void createNetworkNodesAndEdges(JSONArray nodesArray, JSONArray edgesArray, ProjectPackage pkg,
       ProjectPackage parentPkg) throws JSONException
   {
@@ -50,11 +81,11 @@ public class SsaServlet extends HttpServlet
     for (ProjectSrcFile srcFile : srcFiles)
     {
       JSONObject srcNode = new JSONObject();
-      String filename = srcFile.getName();
+      String filename = srcFile.getClassName() + "." + srcFile.getFileExt();
       String srcId = pkgName + "." + filename;
       srcNode.put("id", srcId);
       srcNode.put("label", filename);
-      srcNode.put("title", "Class: <b>" + filename + "</b><br>SLOC: <b>" + srcFile.getSlocCount() + "</b>");
+      srcNode.put("title", "Class: <b>" + srcFile.getClassName() + "</b><br>SLOC: <b>" + srcFile.getSlocCount() + "</b>");
       srcNode.put("group", pkgId);
       srcNode.put("shape", "image");
       srcNode.put("image", "images/java_file.png");
@@ -71,36 +102,5 @@ public class SsaServlet extends HttpServlet
     {
       createNetworkNodesAndEdges(nodesArray, edgesArray, subPkg, pkg);
     }
-  }
-
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-  {
-    ProjectStructure project = FileIO.read(new File(getServletContext().getRealPath(
-        "data/ssa/CodeWeb_SSA_2016-02-06 21-31-51.ssa")));
-
-    PrintWriter out = resp.getWriter();
-
-    JSONObject responseObj = new JSONObject();
-    try
-    {
-      JSONArray nodesArray = new JSONArray();
-      JSONArray edgesArray = new JSONArray();
-      Collection<ProjectPackage> packages = project.getTopPackages();
-      for (ProjectPackage pkg : packages)
-      {
-        createNetworkNodesAndEdges(nodesArray, edgesArray, pkg, null);
-      }
-      responseObj.put("projName", project.getProjName());
-      responseObj.put("nodes", nodesArray);
-      responseObj.put("edges", edgesArray);
-    }
-    catch (JSONException e)
-    {
-      throw new ServletException(e);
-    }
-
-    out.write(responseObj.toString());
-    out.flush();
   }
 }
